2376f1df8e67a28e2c4dedcbff46467ff76bd294
ahmeddsalla <ahmed.d@salla.sa>
2024-04-29 09:08:02 +0300

    init

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+from app import create_app, db
+
+def test_home_page(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/')
+    assert response.status_code == 200
